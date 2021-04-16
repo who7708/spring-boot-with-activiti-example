8d451340914bdcca1acc1f314b61b54879fa6e99
Chris <who7708@163.com>
2021-04-16 21:53:41 +0800

    format code

--- src/main/java/org/activiti/HireProcessRestController.java
+++ src/main/java/org/activiti/HireProcessRestController.java
@@ -4,7 +4,11 @@ import org.activiti.engine.RuntimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Map;
--- src/main/java/org/activiti/MyApp.java
+++ src/main/java/org/activiti/MyApp.java
@@ -15,28 +15,29 @@ public class MyApp {
         SpringApplication.run(MyApp.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner init(final RepositoryService repositoryService,
-//                                  final RuntimeService runtimeService,
-//                                  final TaskService taskService) {
-//
-//        return new CommandLineRunner() {
-//            @Override
-//            public void run(String... strings) throws Exception {
-//                Map<String, Object> variables = new HashMap<String, Object>();
-//                variables.put("applicantName", "John Doe");
-//                variables.put("email", "john.doe@activiti.com");
-//                variables.put("phoneNumber", "123456789");
-//                runtimeService.startProcessInstanceByKey("hireProcess", variables);
-//            }
-//        };
-//
-//    }
+    //    @Bean
+    //    public CommandLineRunner init(final RepositoryService repositoryService,
+    //                                  final RuntimeService runtimeService,
+    //                                  final TaskService taskService) {
+    //
+    //        return new CommandLineRunner() {
+    //            @Override
+    //            public void run(String... strings) throws Exception {
+    //                Map<String, Object> variables = new HashMap<String, Object>();
+    //                variables.put("applicantName", "John Doe");
+    //                variables.put("email", "john.doe@activiti.com");
+    //                variables.put("phoneNumber", "123456789");
+    //                runtimeService.startProcessInstanceByKey("hireProcess", variables);
+    //            }
+    //        };
+    //
+    //    }
 
     @Bean
     InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
 
         return new InitializingBean() {
+            @Override
             public void afterPropertiesSet() throws Exception {
 
                 Group group = identityService.newGroup("user");
